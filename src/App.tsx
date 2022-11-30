da828c7bdb536e6d92d3b61542699cacc339dd3f
Katopuk <ladno4ka@gmail.com>
2022-11-30 22:42:03 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Accordion } from './components/Accordion';
+import { Rating } from './components/Rating';
+import { PageTitle } from './components/PageTitle';
+
 import './App.css';
 
-
-function App() {
-
-
-    return (
-        <div className="App">
-
-        </div>
-    );
+export const App = () => {
+  return (
+    <div>
+      <PageTitle title={"This is component"} />
+      <Rating rating={4}/>
+      <Accordion title={"Menu"} collapsed={false} />
+      <Accordion title={"Navigation"} collapsed={true} />
+      <Rating rating={2}/>
+    </div>
+  );
 }
-
-export default App;
